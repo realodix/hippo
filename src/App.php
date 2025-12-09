6186bcc650bb8bcae33ededc18d81a0b37cb2c55
Kei <budi03@gmail.com>
2025-12-10 03:32:24 +0700

    fix version

--- src/App.php
+++ src/App.php
@@ -7,7 +7,7 @@ use Illuminate\Container\Container;
 class App
 {
     const NAME = 'Haiku';
-    const VERSION = '1.0';
+    const VERSION = '1.1';
 
     /**
      * Register any application services.
