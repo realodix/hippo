549ed73643023d86651d0cb6c35125b7630bb458
Kei <budi03@gmail.com>
2025-12-24 10:23:12 +0700

    Release 1.3

--- src/App.php
+++ src/App.php
@@ -7,7 +7,7 @@ use Illuminate\Container\Container;
 class App
 {
     const NAME = 'Haiku';
-    const VERSION = '1.2';
+    const VERSION = '1.3';
 
     /**
      * Register any application services.
