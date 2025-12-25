479be5910a74065cb931ff0dad40bb798967eb73
Kei <budi03@gmail.com>
2025-12-26 01:21:31 +0700

    formatting

--- src/Fixer/Regex.php
+++ src/Fixer/Regex.php
@@ -5,37 +5,48 @@ namespace Realodix\Haiku\Fixer;
 final class Regex
 {
     /**
-     * @var string Regex to capture the filter body and its options.
-     *             Example: `||example.com^$script,domain=example.org`
+     * Regex to capture the filter body and its options.
+     *
+     * Example: ||example.com^$script,domain=example.org
+     *
+     * @var string
      */
     // const NET_OPTION = '/^(.*)\$(~?[\w\-]+(?:=[^,\s]+)?(?:,~?[\w\-]+(?:=[^,\s]+)?)*)$/';
     // const NET_OPTION = '/^(.*)\$(~?[\w\-]+(?:=[^\s]+)?(?:,~?[\w\-]+(?:=[^\s]+)?)*)$/';
     const NET_OPTION = '/^(.*)\$(~?[\w\-]+(?:=.+)?(?:,~?[\w\-]+(?:=.+)?)*)$/';
 
     /**
-     * @var string Regex to find domain-related options in a network filter.
+     * Regex to find domain-related options in a network filter.
+     *
+     * @var string
      */
     const NET_OPTION_DOMAIN = '/(?:\$|,)(?:denyallow|domain|from|method|to)\=([^,\s]+)$/';
 
     /**
-     * https://regex101.com/r/yY3a26/2
+     * Regex to identify and capture parts of an element hiding rule.
      *
-     * @var string Regex to identify and capture parts of an element hiding rule.
-     *             Example: `example.com,example.org##.ad`
+     * Example: example.com,example.org##.ad
+     * Ref: https://regex101.com/r/yY3a26/2
+     *
+     * @var string
      */
     const COSMETIC_RULE = '/^(?:\[\$[^\]]+\])?([^\/\|\@\"\!]*?|\/.+\/)(#@?[$?]{1,2}#|#@?#[\^\+]?|\$\@?\$)(.*)$/';
 
     /**
-     * https://regex101.com/r/2E6nAd
+     * Regex to find domains in element-hiding rules.
      *
-     * @var string Regex to find domains in element-hiding rules.
+     * Ref: https://regex101.com/r/2E6nAd
+     *
+     * @var string
      */
     const COSMETIC_DOMAIN = '/^(?!##?\s)([^\/\|\@\"!]*?)(##|#[@?$%]{1,3}#|\$@?\$)/';
 
     /**
-     * https://regex101.com/r/NYYNxd
+     * Regex to find AdGuard JS rules.
      *
-     * @var string Regex to find AdGuard JS rules.
+     * Ref: https://regex101.com/r/NYYNxd
+     *
+     * @var string
      */
     const AG_JS_RULE = '/^(?:\[\$[^\]]+\])?([^\/\|\@\"\!]*?)(#@?%#[\^\+]?)(.*)$/';
 }
