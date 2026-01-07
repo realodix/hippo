163476740943c73b5b014eccdc25b069efca03f8
Kei <budi03@gmail.com>
2026-01-07 17:00:54 +0700

    Reverting release version changes

--- src/App.php
+++ src/App.php
@@ -7,7 +7,7 @@ use Illuminate\Container\Container;
 class App
 {
     const NAME = 'Haiku';
-    const VERSION = '1.4.0';
+    const VERSION = '1.4.x-dev';
 
     /**
      * Register any application services.
